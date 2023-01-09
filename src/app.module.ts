c94bde27f21f10d93fb17fcb4d13b89ecf9e774d
Карабаев Нуриддин <94254058+asuramesocrum@users.noreply.github.com>
2023-01-10 03:06:43 +0400

    init

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { CacheModule, Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PrismaService } from './prisma.service';
+
+// import * as bcrypt from 'bcrypt';
+
+@Module({
+  imports: [
+    // SERVER IMPORTS
+    // constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+    CacheModule.register({
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    // CUSTOM MODULE IMPORT
+    // ...
+  ],
+  controllers: [],
+  providers: [
+    PrismaService,
+  ],
+})
+export class AppModule {
+}
